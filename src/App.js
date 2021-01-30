5306eab794100b0fb76bfa7fa47c441039690f00
JEAguilera <77275656+JEAguilera@users.noreply.github.com>
2021-01-29 23:00:42 -0300

    HyS
    This is a training excercise of React.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <br/>
+      <h1> Higiene y Seguridad: Elementos de Proteccion Personal </h1>
+      <div>
+        <br/>
+            <p> Somos proveedores de elementos de seguiridad laboral. Contactanos para solicitar cotizacion. </p>
+      </div>     
+
+       
+    </div>
+  );
+}
+
+export default App;
